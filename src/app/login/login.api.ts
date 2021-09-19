109d1cd47adff59a927d7616c3b319792696037e
ViniciusNino <viniciussanvas@gmail.com>
2021-09-19 17:06:19 -0300

    refect login

---
+++ src/app/login/login.api.ts
@@ -0,0 +1,18 @@
+import { Injectable } from "@angular/core";
+import { HttpProvider } from "../shared/providers/http.providers";
+import { IDadosAutenticacao, IUsuario } from "../shared/usuario/interfaces";
+import { Login } from "./login.endpoints";
+
+@Injectable({
+    providedIn: 'root'
+  })
+
+export class LoginApi {
+    constructor(readonly http: HttpProvider) {}
+
+    async getUsuarioAutenticado(dadosAutenticacao: IDadosAutenticacao): Promise<IUsuario> {
+        const body = dadosAutenticacao;
+
+        return this.http.get<IUsuario>(Login.AUTENTICAR, body).toPromise();
+    }
+}
--- src/app/login/login.page.ts
+++ src/app/login/login.page.ts
@@ -1,7 +1,10 @@
 import { Component, OnInit } from '@angular/core';
 import { Router } from '@angular/router';
-import { HttpClient } from '@angular/common/http';
-import { JsonPipe } from '@angular/common';
+import { API} from '../../environments/environment';
+import { LoginApi } from './login.api';
+import { IDadosAutenticacao, IUsuario } from '../shared/usuario/interfaces';
+import { TipoPerfilUsuario } from '../shared/usuario/enums';
+import { USUARIO } from '../shared/usuario/constants';
 
 @Component({
   selector: 'app-login',
@@ -9,29 +12,35 @@ import { JsonPipe } from '@angular/common';
   styleUrls: ['./login.page.scss'],
 })
 export class LoginPage implements OnInit {
-  userData = { "sUsuario": "", "sSenha": "" };
-  private API_URL = 'http://localhost:5000/'
-  constructor(private router :Router,private http: HttpClient) {
-    
-   }
+  API_URL = API;
+  usuario: IUsuario;
+  dadosAutenticacao: IDadosAutenticacao;
+  
+  constructor(
+    private router :Router,
+    readonly logimApi: LoginApi,
+    ) { }
 
-  ngOnInit() {
+  ngOnInit() { 
+    this.dadosAutenticacao = {Usuario: "", Senha: ""}
   }
-  public login(form)
-  {
-    this.http.post(this.API_URL+"usuario/autenticar",this.userData).subscribe((response) => {
-      if (response ["nIdUsuarioAutenticado"] > 0) {
-             localStorage.setItem('userData', JSON.stringify(response));
-             if(response["nIdPerfilUsuario"] == 4 ){
-              this.router.navigateByUrl("home-solicitante");
-             }
-             else{
-                this.router.navigateByUrl("home"); 
 
-             }
-           } else {
-             alert("Usuário ou Senha Inválido")
-          }
-    });               
-  }
+  async login() {
+    this.usuario = await this.logimApi
+      .getUsuarioAutenticado(this.dadosAutenticacao)
+      .catch(error => {
+        console.error(error);
+        return null;
+      })
+    if (this.usuario.id > 0) {
+      localStorage.setItem(USUARIO.USUARIOAUTENTICAR, JSON.stringify(this.usuario));
+      if(this.usuario.perfilId == TipoPerfilUsuario.Solicitante ) {
+        this.router.navigateByUrl("home-solicitante");
+      } else {
+        this.router.navigateByUrl("home"); 
+      }
+    } else {
+      alert("Usuário ou Senha Inválido")
+    }
+  }  
 }
